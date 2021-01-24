b092e2644d4082b65ff896612c931dfbf32b119e
olegwt <66882866+olegwt@users.noreply.github.com>
2021-01-24 17:29:21 +0300

    Lesson #3

--- my_site1_django/my_site1_django/settings.py
+++ my_site1_django/my_site1_django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'realtyestateapp.apps.RealtyestateappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
